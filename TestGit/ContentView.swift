e63ff02385851dd7341c84d15a4050d864bce197
88DB HK <riverleung@88db.com.hk>
2020-03-27 10:56:15 +0800

    Testing1

---
+++ TestGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by Chun Wah Leung on 27/3/2020.
+//  Copyright © 2020 LEUNG CHUN WAH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!") 
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
