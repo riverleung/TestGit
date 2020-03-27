2e8555dc8609cf28ec5460fdbda22a1256b2e60e
88DB HK <riverleung@88db.com.hk>
2020-03-27 10:59:09 +0800

    fsdfdsfsd

--- TestGit/AppDelegate.swift
+++ TestGit/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("ssdsdsdsds")
+        print("ssdsdsddfdsfsdfsds")
         // Override point for customization after application launch.
         return true
     }
